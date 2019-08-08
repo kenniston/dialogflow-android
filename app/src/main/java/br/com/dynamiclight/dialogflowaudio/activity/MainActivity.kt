76e075318e0fc853ea56ab7f7ff37d557e39c071
C1280932 Kenniston Arraes Bonfim <c1280932@bb.com.br>
2019-08-08 02:03:17 -0300

    Audio upload multipart

--- app/src/main/java/br/com/dynamiclight/dialogflowaudio/activity/MainActivity.kt
+++ app/src/main/java/br/com/dynamiclight/dialogflowaudio/activity/MainActivity.kt
@@ -1,35 +1,126 @@
 package br.com.dynamiclight.dialogflowaudio.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import br.com.dynamiclight.dialogflowaudio.R
 import br.com.dynamiclight.dialogflowaudio.model.DialogflowResult
 import br.com.dynamiclight.dialogflowaudio.repository.DialogflowRepository
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.IOException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var recording = false
+    private val WRITE_REQUEST_CODE = 9998
+    private val RECORD_REQUEST_CODE = 9997
+    private val uuid = UUID.randomUUID().toString()
+
+    lateinit var recorder: MediaRecorder
+    lateinit var file: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DialogflowRepository(this).sentTextMessage(
-            "Olá", "kenniston@gmail.com",
-            UUID.randomUUID().toString(),
-            object : Callback<DialogflowResult>
-            {
+        checkPermission()
+
+        imgRecord.setOnClickListener { startRecorder() }
+    }
+
+    private fun startRecorder() {
+        recording = !recording
+
+        if (recording) {
+            imgRecord.setImageResource(R.drawable.btn2)
+        } else {
+            imgRecord.setImageResource(R.drawable.btn1)
+        }
+
+        if (recording) {
+            recorder = MediaRecorder()
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP) //MPEG_4
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            val path = File(Environment.getExternalStorageDirectory().path)
+            try {
+                file = File.createTempFile("temporary", ".3gp", path)
+            } catch (e: IOException) {
+                Toast.makeText(this, e.localizedMessage, Toast.LENGTH_LONG).show()
+                return
+            }
+
+            recorder.setOutputFile(file.absolutePath)
+            try {
+                recorder.prepare()
+            } catch (e: IOException) {
+                Toast.makeText(this, e.localizedMessage, Toast.LENGTH_LONG).show()
+                return
+            }
+            recorder.start()
+        } else {
+
+            recorder.stop()
+
+            DialogflowRepository(this).sentAudioMessage(file.absolutePath, "kenniston@gmail.com", uuid, object : Callback<DialogflowResult> {
                 override fun onResponse(call: Call<DialogflowResult>, response: Response<DialogflowResult>) {
-                    Log.d("DIALOGFLOW", response.body().toString())
+                    Toast.makeText(this@MainActivity, "SUCESSO", Toast.LENGTH_LONG).show()
                 }
 
                 override fun onFailure(call: Call<DialogflowResult>, t: Throwable) {
                     Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_LONG).show()
                 }
             })
+        }
     }
+
+    private fun checkPermission() {
+        var permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_REQUEST_CODE)
+        }
+
+        permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            WRITE_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "A permissão de acesso ao dispositivo é necessária.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            RECORD_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "A permissão para gravação de áudio é necessária.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/br/com/dynamiclight/dialogflowaudio/repository/DialogflowRepository.kt
+++ app/src/main/java/br/com/dynamiclight/dialogflowaudio/repository/DialogflowRepository.kt
@@ -4,18 +4,24 @@ import android.content.Context
 import br.com.bb.valentino.repository.DialogflowRetrofit
 import br.com.dynamiclight.dialogflowaudio.model.DialogflowRequest
 import br.com.dynamiclight.dialogflowaudio.model.DialogflowResult
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
+import java.io.File
+
 
 interface DialogflowService {
 
-    @POST("/message/text/send")
+    @POST("message/text/send")
     @Headers("Content-Type: application/json")
     fun sendTextMessage(@Body request: DialogflowRequest): Call<DialogflowResult>
 
+    @Multipart
+    @POST("message/audio/send")
+    fun sendAudioMessage(@Part("json") request: RequestBody, @Part file: MultipartBody.Part): Call<DialogflowResult>
 }
 
 class DialogflowRepository(context: Context) : DialogflowRetrofit(context) {
@@ -24,5 +30,17 @@ class DialogflowRepository(context: Context) : DialogflowRetrofit(context) {
     fun sentTextMessage(text: String, email: String, sessionId: String, callback: Callback<DialogflowResult>) {
         val request = DialogflowRequest(text, email, sessionId)
         service.sendTextMessage(request).enqueue(callback)
+    }
+
+    fun sentAudioMessage(filePath: String, email: String, sessionId: String, callback: Callback<DialogflowResult>) {
+        val request = DialogflowRequest("", email, sessionId)
+        val data = RequestBody.create(MediaType.parse("application/json"), toJson(request))
+
+        val file = File(filePath)
+        val fileData = RequestBody.create(MediaType.parse("audio/3gpp"), file)
+
+        val part = MultipartBody.Part.createFormData("audioFile", file.name, fileData)
+
+        service.sendAudioMessage(data, part).enqueue(callback)
     }
 }
